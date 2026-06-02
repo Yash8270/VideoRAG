879c1e7a08378775e18f693f0b8c0dbadffb8968
Yash8270 <yashlimbachiya900@gmail.com>
2026-06-03 00:20:50 +0530

    Correction regarding CORS policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -96,7 +96,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=_settings.ALLOWED_ORIGINS,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
